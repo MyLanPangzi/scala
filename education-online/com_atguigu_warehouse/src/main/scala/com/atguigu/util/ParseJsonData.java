8ec7474234debfa5933ef49ee9f0e5b0776ffd29
1251723871@qq.com <smailXie@163.com>
2020-06-17 17:37:22 +0800

    feat(online-edu):add dwd etl

---
+++ education-online/com_atguigu_warehouse/src/main/scala/com/atguigu/util/ParseJsonData.java
@@ -0,0 +1,17 @@
+package com.atguigu.util;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+
+public class ParseJsonData {
+
+    public static JSONObject getJsonData(String data) {
+        try {
+            return JSONObject.parseObject(data);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
